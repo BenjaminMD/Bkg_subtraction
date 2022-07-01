b206c268fd208c2c3cbcfd35854962131751d91e
ifk <ifk.rwth@gamil.com>
2022-07-01 10:04:52 +0200

    fixed formatting

--- bkg_shift.py
+++ bkg_shift.py
@@ -1,8 +1,8 @@
 from scipy.optimize import curve_fit
 import numpy as np
 
-catalyst_path = './dat/LaCe-NiO4_accum_r01.xyd' 
-support_path =  './dat/LaCe600_accum_r01.xyd'
+catalyst_path = './dat/LaCe-NiO4_accum_r01.xyd'
+support_path = './dat/LaCe600_accum_r01.xyd'
 
 name = support_path.split('/')[-1].split('.')[0]
 
@@ -17,21 +17,23 @@ catalyst_peak = catalyst[mask]
 support_peak = support[mask]
 
 
-mask  = q_peak > q_min
+mask = q_peak > q_min
 
 q_peak = q_peak[mask]
 catalyst_peak = catalyst_peak[mask]
 support_peak = support_peak[mask]
 
 
-
-
-
 def gauss(q, mu, A, sigma):
     return A*np.exp(-(q-mu)**2/(2.*sigma**2))
 
-(mu_catalyst, *_), (*_) = curve_fit(gauss, q_peak, catalyst_peak, maxfev=int(1e6))
-(mu_support, *_), (*_) = curve_fit(gauss, q_peak, support_peak, maxfev=int(1e6))
+
+(mu_catalyst, *_), (*_) = curve_fit(
+        gauss, q_peak, catalyst_peak, maxfev=int(1e6)
+    )
+(mu_support, *_), (*_) = curve_fit(
+        gauss, q_peak, support_peak, maxfev=int(1e6)
+    )
 stretch_in_q = mu_catalyst / mu_support
 
 
